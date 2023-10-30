79e1f7910e036ac70482ec3c2597767d9dba55b7
heasp04 <heasp2004@gmail.com>
2023-10-30 23:19:42 +0100

    Made something of a dialogue system

--- Assets/Scripts/LineGenerator.cs
+++ Assets/Scripts/LineGenerator.cs
@@ -7,36 +7,40 @@ public class LineGenerator : MonoBehaviour
 {
     public GameObject linePrefab;
     private Line _activeLine;
+    private bool _isMouseOverDrawable = false;    
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            GameObject newLine = Instantiate(linePrefab);
-            _activeLine = newLine.GetComponent<Line>();
-        }
+        //if (_isMouseOverDrawable)
+        //{
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject newLine = Instantiate(linePrefab);
+                _activeLine = newLine.GetComponent<Line>();
+            }
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            _activeLine = null;
-        }
+            if (Input.GetMouseButtonUp(0))
+            {
+                _activeLine = null;
+            }
 
-        if (_activeLine != null)
-        {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            _activeLine.UpdateLine(mousePos);
-        }
+            if (_activeLine != null)
+            {
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                _activeLine.UpdateLine(mousePos);
+            }
+        //}
     }
 
     private void OnMouseExit()
     {
-        throw new NotImplementedException();
+        if (gameObject.CompareTag("Drawable")) _isMouseOverDrawable = false;
     }
 
     private void OnMouseOver()
     {
-        throw new NotImplementedException();
+        if (gameObject.CompareTag("Drawable")) _isMouseOverDrawable = true;
     }
-
     
 }
