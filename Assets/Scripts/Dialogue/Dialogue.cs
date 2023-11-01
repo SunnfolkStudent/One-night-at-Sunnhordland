f4856ae855439dfb91c41719f2bbb8e859ecbae5
heasp04 <heasp2004@gmail.com>
2023-11-01 11:46:56 +0100

    I have no idea-

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
-public class Dialogue
+public class Dialogue : MonoBehaviour
 {
     public string name;
     
     [TextArea(1, 10)]
     public string[] sentences;
 
-
-
+    public GameObject dialogueElement;
+    
+    
+    
+    
 }
--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public GameObject lineGenerator;
 
     public void TriggerDialogue()
     {
+        lineGenerator = null;
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    public void DisableElement()
+    {
+        if (lineGenerator == null)
+        {
+            lineGenerator.SetActive(false);
+        }
     }
 }
