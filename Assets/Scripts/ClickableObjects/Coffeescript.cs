9c3a94cb6f1d4dae3583ea1897674ecd8bfabc3a
EOppedal <erlend.oppedal@gmail.com>
2023-11-02 13:30:08 +0100

    coffee unavailabe

--- Assets/Scripts/ClickableObjects/Coffeescript.cs
+++ Assets/Scripts/ClickableObjects/Coffeescript.cs
@@ -6,6 +6,7 @@ namespace ClickableObjects
     {
         private SleepMeter _sleepMeter;
         [SerializeField] private float coffeeTimeRegeneration;
+        private bool _coffeeAvailable;
 
         private void Start()
         {
@@ -14,7 +15,10 @@ namespace ClickableObjects
 
         private void OnMouseDown()
         {
+            if (_coffeeAvailable == false) return;
+            
             _sleepMeter.IncreaseTimer(5);
+            _coffeeAvailable = false;
         }
     }
 }
